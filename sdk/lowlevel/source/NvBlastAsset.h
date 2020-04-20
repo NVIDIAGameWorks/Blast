0de1b9af05987b80b9ff08a0185cf3c4177cfab7
Bryan Galdrikian <bgaldrikian@nvidia.com>
2020-04-20 09:00:26 -0700

    Fixing unit tests

--- sdk/lowlevel/source/NvBlastAsset.h
+++ sdk/lowlevel/source/NvBlastAsset.h
@@ -304,7 +304,7 @@ NV_INLINE uint32_t Asset::getContiguousLowerSupportIndex(uint32_t chunkIndex) co
 
 
 //JDM: Expose this so serialization layer can use it.
-NVBLAST_API Asset* initializeAsset(void* mem, NvBlastID id, uint32_t chunkCount, uint32_t graphNodeCount, uint32_t leafChunkCount, uint32_t firstSubsupportChunkIndex, uint32_t bondCount, NvBlastLog logFn);
+NVBLAST_API Asset* initializeAsset(void* mem, uint32_t chunkCount, uint32_t graphNodeCount, uint32_t leafChunkCount, uint32_t firstSubsupportChunkIndex, uint32_t bondCount, NvBlastLog logFn);
 
 } // namespace Blast
 } // namespace Nv
