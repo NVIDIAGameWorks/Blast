f7e87b44aa1ebec0229a8e8d8c5308e61621437e
Bryan Galdrikian <bgaldrikian@nvidia.com>
2021-10-18 21:43:43 -0700

    Changing back to debug build for testing

--- tools/ci/testing/test-windows-x86_64/gtests.bat
+++ tools/ci/testing/test-windows-x86_64/gtests.bat
@@ -1,3 +1,3 @@
-@call "%~dp0..\..\..\..\_build\windows-x86_64\release\blast-sdk\bin\UnitTests.exe" --gtest_output=xml:"%~dp0..\..\..\..\_build\windows-x86_64\release\blast-sdk\bin\UnitTests.xml"
-@echo ##teamcity[importData type='gtest' parseOutOfDate='true' file='%~dp0..\..\..\..\_build\windows-x86_64\release\blast-sdk\bin\UnitTests.xml']
+@call "%~dp0..\..\..\..\_build\windows-x86_64\debug\blast-sdk\bin\UnitTests.exe" --gtest_output=xml:"%~dp0..\..\..\..\_build\windows-x86_64\debug\blast-sdk\bin\UnitTests.xml"
+@echo ##teamcity[importData type='gtest' parseOutOfDate='true' file='%~dp0..\..\..\..\_build\windows-x86_64\debug\blast-sdk\bin\UnitTests.xml']
 @if %errorlevel% NEQ 0 exit /b 1
