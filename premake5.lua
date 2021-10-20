e51922c79e16f4734662c2fc93136ee1f6296443
Bryan Galdrikian <bgaldrikian@nvidia.com>
2021-10-20 11:05:54 -0700

    Linux building, packaging, publishing, and tests working and passing chmod +x tools/packman/packman

--- premake5.lua
+++ premake5.lua
@@ -572,6 +572,11 @@ group "sdk"
             ["include/*"] = "include/extensions/serialization/",
             ["source/*"] = "source/sdk/extensions/serialization/",
         }
+        filter { "system:linux"}
+            disablewarnings {
+                "undef",
+                "sign-compare"
+            }
 
         -- requires FBX SDK.  Original SDK only defined this for Windows
     -- project "NvBlastExtExporter"
@@ -673,101 +678,107 @@ group "sdk"
     --         ["source/*"] = "source/sdk/extensions/serialization/",
     --     }
 
--- group "tests"
---     project "UnitTests"
---         kind "ConsoleApp"
---         location (workspaceDir.."/%{prj.name}")
---         link_dependents({"NvBlast", "NvBlastGlobals", "NvBlastExtAssetUtils", "NvBlastExtShaders", "NvBlastTk", "NvBlastExtSerialization", "NvBlastExtTkSerialization"})
+group "tests"
+    project "UnitTests"
+        kind "ConsoleApp"
+        location (workspaceDir.."/%{prj.name}")
+        link_dependents({"NvBlast", "NvBlastGlobals", "NvBlastExtAssetUtils", "NvBlastExtShaders", "NvBlastTk", "NvBlastExtSerialization", "NvBlastExtTkSerialization"})
 
---         filter { "system:windows" }
---             -- defines { "ISOLATION_AWARE_ENABLED=1" }
---         filter { "system:linux" }
---             buildoptions { "-fPIC" }
---             links { "rt" }
---         filter{}
+        filter { "system:windows" }
+            -- defines { "ISOLATION_AWARE_ENABLED=1" }
+        filter { "system:linux" }
+            buildoptions { "-fPIC" }
+            links { "rt" }
+        filter{}
 
---         blast_sdklib_common_files()
+        blast_sdklib_common_files()
 
---         add_files("source/test/src/unit", {
---             "AssetTests.cpp",
---             "ActorTests.cpp",
---             "APITests.cpp",
---             "CoreTests.cpp",
---             "FamilyGraphTests.cpp",
---             "MultithreadingTests.cpp",
---             "TkCompositeTests.cpp",
---             "TkTests.cpp",
---         })
+        add_files("source/test/src/unit", {
+            "AssetTests.cpp",
+            "ActorTests.cpp",
+            "APITests.cpp",
+            "CoreTests.cpp",
+            "FamilyGraphTests.cpp",
+            "MultithreadingTests.cpp",
+            "TkCompositeTests.cpp",
+            "TkTests.cpp",
+        })
 
---         add_files("source/test/src/utils", {
---             "TestAssets.cpp",
---         })
+        add_files("source/test/src/utils", {
+            "TestAssets.cpp",
+        })
 
---         add_files("source/sdk/lowlevel", {
---             "NvBlastActor.cpp",
---             "NvBlastFamilyGraph.cpp",
---             "NvBlastActorSerializationBlock.cpp",
---             "NvBlastAsset.cpp",
---             "NvBlastFamily.cpp",
---         })
+        add_files("source/sdk/lowlevel", {
+            "NvBlastActor.cpp",
+            "NvBlastFamilyGraph.cpp",
+            "NvBlastActorSerializationBlock.cpp",
+            "NvBlastAsset.cpp",
+            "NvBlastFamily.cpp",
+        })
 
---         add_files("source/shared/utils", {
---             "AssetGenerator.cpp",
---         })
+        add_files("source/shared/utils", {
+            "AssetGenerator.cpp",
+        })
 
---         add_files("source/sdk/extensions/physx", {  -- !!!
---             "NvBlastExtPxTaskImpl.cpp",
---         })
+        add_files("source/sdk/extensions/physx", {  -- !!!
+            "NvBlastExtPxTaskImpl.cpp",
+        })
 
---         includedirs {
---             "include/globals",
---             "include/lowlevel",
---             "include/toolkit",
---             "include/extensions/assetutils",
---             "include/extensions/physx", -- !!!
---             "include/extensions/shaders",
---             "include/extensions/serialization",
---             "source/sdk/common",
---             "source/sdk/lowlevel",
---             "source/sdk/extensions/serialization",
---             "source/test/src",
---             "source/test/src/unit",
---             "source/test/src/utils",
---             "source/shared/filebuf/include",
---             "source/shared/utils",
---             target_deps.."/physxsdk/include",
---             target_deps.."/physxsdk/source/foundation/include",
---             target_deps.."/pxshared/include",
---             target_deps.."/googletest/include",
---         }
+        filter { "system:linux" }
+            add_files("source/shared/task", {
+                "TaskManager.cpp"
+            })
 
---     filter { "system:windows", "configurations:debug" }
---         links {
---             target_deps.."/googletest/lib/vc14win64-cmake/Debug/gtest_main.lib",
---             target_deps.."/googletest/lib/vc14win64-cmake/Debug/gtest.lib",
---             target_deps.."/physxsdk/bin/win.x86_64.vc141.md/debug/PhysXFoundation_64.lib",
---             target_deps.."/physxsdk/bin/win.x86_64.vc141.md/debug/PhysXTask_static_64.lib",
---         }
---         repo_build.copy_to_targetdir(target_deps.."/physxsdk/bin/win.x86_64.vc141.md/debug/PhysXFoundation_64.dll")
---     filter { "system:windows", "configurations:release" }
---         links {
---             target_deps.."/googletest/lib/vc14win64-cmake/Release/gtest_main.lib",
---             target_deps.."/googletest/lib/vc14win64-cmake/Release/gtest.lib",
---             target_deps.."/physxsdk/bin/win.x86_64.vc141.md/release/PhysXFoundation_64.lib",
---             target_deps.."/physxsdk/bin/win.x86_64.vc141.md/release/PhysXTask_static_64.lib",
---         }
---         repo_build.copy_to_targetdir(target_deps.."/physxsdk/bin/win.x86_64.vc141.md/release/PhysXFoundation_64.dll")
---     filter { "system:linux" }
---     filter{}
+        includedirs {
+            "include/globals",
+            "include/lowlevel",
+            "include/toolkit",
+            "include/extensions/assetutils",
+            "include/extensions/physx", -- !!!
+            "include/extensions/shaders",
+            "include/extensions/serialization",
+            "source/sdk/common",
+            "source/sdk/lowlevel",
+            "source/sdk/extensions/serialization",
+            "source/test/src",
+            "source/test/src/unit",
+            "source/test/src/utils",
+            "source/shared/filebuf/include",
+            "source/shared/utils",
+            target_deps.."/physxsdk/include",
+            target_deps.."/physxsdk/source/foundation/include",
+            target_deps.."/pxshared/include",
+            target_deps.."/googletest/include",
+        }
 
---     filter { "system:windows" }
---         disablewarnings {
---             "4002", -- too many actual parameters for macro 'identifier'
---             "4100", -- unreferenced formal parameter
---             "4127", -- conditional expression is constant
---             "4189", -- 'identifier' : local variable is initialized but not referenced
---             "4244", -- conversion from 'type1' to 'type2', possible loss of data
---             "4456", -- declaration of 'identifier' hides previous local declaration
---             "4996", -- code uses a function, class member, variable, or typedef that's marked deprecated
---         }
---     filter {}
+    filter { "system:windows", "configurations:debug" }
+        libdirs { target_deps.."/googletest/lib/vc14win64-cmake/Debug", target_deps.."/physxsdk/bin/win.x86_64.vc141.md/debug" }
+    filter { "system:windows", "configurations:release" }
+        libdirs { target_deps.."/googletest/lib/vc14win64-cmake/Release", target_deps.."/physxsdk/bin/win.x86_64.vc141.md/release" }
+    filter { "system:linux", "configurations:debug" }
+        libdirs { target_deps.."/googletest/lib/gcc-4.8", target_deps.."/physxsdk/bin/linux.clang/debug" }
+    filter { "system:linux", "configurations:release" }
+        libdirs { target_deps.."/googletest/lib/gcc-4.8", target_deps.."/physxsdk/bin/linux.clang/debug" }
+    filter{}
+
+    links { "gtest_main", "gtest" }
+
+    filter { "system:windows" }
+        links { "PhysXFoundation_64", "PhysXTask_static_64" }
+        repo_build.copy_to_targetdir(target_deps.."/physxsdk/bin/win.x86_64.vc141.md/%{config}/PhysXFoundation_64.dll")
+        disablewarnings {
+            "4002", -- too many actual parameters for macro 'identifier'
+            "4100", -- unreferenced formal parameter
+            "4127", -- conditional expression is constant
+            "4189", -- 'identifier' : local variable is initialized but not referenced
+            "4244", -- conversion from 'type1' to 'type2', possible loss of data
+            "4456", -- declaration of 'identifier' hides previous local declaration
+            "4996", -- code uses a function, class member, variable, or typedef that's marked deprecated
+        }
+    filter { "system:linux"}
+        links { "PhysXFoundation_static_64" }
+        disablewarnings {
+            "undef",
+            "sign-compare"
+        }
+    filter {}
