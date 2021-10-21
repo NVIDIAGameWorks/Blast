928b2df7a5c8f5c9ddc3d2ea4754a7e4fd823344
Bryan Galdrikian <bgaldrikian@nvidia.com>
2021-10-20 17:45:01 -0700

    Hammering +x on UnitTests since TC's unzip loses  file permissions

--- tools/ci/testing/test-linux-x86_64/gtests.sh
+++ tools/ci/testing/test-linux-x86_64/gtests.sh
@@ -6,6 +6,7 @@ set -e
 bin_path="$(dirname "$0")/../../../../_build/linux-x86_64/release/blast-sdk/bin"
 pushd $bin_path
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+chmod +x UnitTests # it turns out that TC's unzip loses the file permissions :/
 ./UnitTests --gtest_output=xml:UnitTests.xml
 echo \#\#teamcity[importData type=\'gtest\' parseOutOfDate=\'true\' file=\'UnitTests.xml\']
 popd
