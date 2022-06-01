e2dab873d45ac3f37f47f900db2fe74b330b1e78
Bryan Galdrikian <bgaldrikian@nvidia.com>
2022-05-31 23:11:49 -0700

    SIMD device query for gcc

--- source/shared/stress_solver/simd/simd_device_query.h
+++ source/shared/stress_solver/simd/simd_device_query.h
@@ -1,33 +1,5 @@
-// This code contains NVIDIA Confidential Information and is disclosed to you
-// under a form of NVIDIA software license agreement provided separately to you.
-//
-// Notice
-// NVIDIA Corporation and its licensors retain all intellectual property and
-// proprietary rights in and to this software and related documentation and
-// any modifications thereto. Any use, reproduction, disclosure, or
-// distribution of this software and related documentation without an express
-// license agreement from NVIDIA Corporation is strictly prohibited.
-//
-// ALL NVIDIA DESIGN SPECIFICATIONS, CODE ARE PROVIDED "AS IS.". NVIDIA MAKES
-// NO WARRANTIES, EXPRESSED, IMPLIED, STATUTORY, OR OTHERWISE WITH RESPECT TO
-// THE MATERIALS, AND EXPRESSLY DISCLAIMS ALL IMPLIED WARRANTIES OF NONINFRINGEMENT,
-// MERCHANTABILITY, AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// Information and code furnished is believed to be accurate and reliable.
-// However, NVIDIA Corporation assumes no responsibility for the consequences of use of such
-// information or for any infringement of patents or other rights of third parties that may
-// result from its use. No license is granted by implication or otherwise under any patent
-// or patent rights of NVIDIA Corporation. Details are subject to change without notice.
-// This code supersedes and replaces all information previously supplied.
-// NVIDIA Corporation products are not authorized for use as critical
-// components in life support devices or systems without express written approval of
-// NVIDIA Corporation.
-//
-// Copyright (c) 2022 NVIDIA Corporation. All rights reserved.
-
 #pragma once
 
-#include <intrin.h>
 #include <bitset>
 #include <array>
 #include <utility>
@@ -88,18 +60,29 @@ constexpr std::pair<const char*, uint32_t> sInstructionSetLookup[] =
 };
 
 
+#if NV_WINDOWS_FAMILY
+#include <intrin.h>
+inline void cpuid(int cpui[4], int fn) { __cpuidex(cpui, fn, 0); }
+#else
+#include <cpuid.h>
+inline void cpuid(int cpui[4], int fn) { __cpuid_count(fn, 0, cpui[0], cpui[1], cpui[2], cpui[3]); }
+#endif
+
+
 static bool
 device_supports_instruction_set(uint32_t inst_set)
 {
     int fn, bitset, bit;
     extractInstSetBitsetAndBit(fn, bitset, bit, inst_set);
 
-    std::array<int, 4> cpui;
-    __cpuid(cpui.data(), 0);
+    int cpui[4];
+    cpuid(cpui, 0);
+
     if (cpui[0] < fn) return false;
 
-    __cpuidex(cpui.data(), fn, 0);
-    return std::bitset<32>(cpui[bitset])[bit];
+    cpuid(cpui, fn);
+
+    return !!((cpui[bitset] >> bit) & 1);
 }
 
 static void
