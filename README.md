33e1cd9363e879a71fd5a56a47d37659a250354d
Bryan Galdrikian <bgaldrikian@nvidia.com>
2022-12-07 10:23:21 -0800

    More tweaks to public repo copy

--- README.md
+++ README.md
@@ -5,20 +5,10 @@ Online documentation may be found here: [Blast SDK Documentation](http://omniver
 ## Building the SDK
 
 ### Windows
-1. build: run `build.bat`
+1. run `build.bat`
 2. built sdk location: `_build\windows-x86_64\release\blast-sdk` (release), `_build\windows-x86_64\debug\blast-sdk` (debug) 
 
 ### Linux
 0. initialize (once): run `./setup.sh`
-1. build: run `./build.sh`
+1. run `./build.sh`
 2. built sdk location: `_build/linux-x86_64/release/blast-sdk` (release), `_build/linux-x86_64/debug/blast-sdk` (debug) 
-
-## Building Documentation
-
-### Windows
-1. build: run `repo.bat docs`
-2. built docs location: `_build\docs\blast-sdk\latest`
-
-### Linux
-1. build: run `./repo.sh docs`
-2. built docs location: `_build/docs/blast-sdk/latest`
--- tools/copy_to_public_repo.bat
+++ tools/copy_to_public_repo.bat
@@ -4,10 +4,12 @@ set dst=%1\blast
 
 RD /S /Q %dst%
 
-robocopy %~dp0.. %dst% /s /ns /nc /ndl /np /xd "_*" ".*" "tools" "exporter" "physx" "RT" "apps" "samples" /xf "NvBlastExtPx*" "NvBlastExtKJPx*" "ExtPx*" "ext_px*" "ext_physx*" "ext_exporter*" "tinyobjloader-LICENSE.md"
+robocopy %~dp0.. %dst% /s /ns /nc /ndl /np /xd "_*" ".*" "docs" "tools" "exporter" "physx" "RT" "apps" "samples" /xf "NvBlastExtPx*" "NvBlastExtKJPx*" "ExtPx*" "ext_px*" "ext_physx*" "ext_exporter*" "tinyobjloader-LICENSE.md"
+robocopy %~dp0..\docs %dst%\docs CHANGELOG.md /ns /nc /ndl /np
 robocopy %~dp0..\include\extensions\physx %dst%\include\extensions\physx NvBlastExtPxTask.h /ns /nc /ndl /np
 robocopy %~dp0..\source\sdk\extensions\physx %dst%\source\sdk\extensions\physx NvBlastExtPxTaskImpl.* /ns /nc /ndl /np
 robocopy %~dp0..\tools\packman %dst%\tools\packman /s /ns /nc /ndl /np
-robocopy %~dp0..\tools\repoman %dst%\tools\repoman /s /ns /nc /ndl /np
+robocopy %~dp0..\tools\repoman %dst%\tools\repoman /s /ns /nc /ndl /np /xd "_*"
 
 call "%~dp0..\tools\packman\python" %~dp0edit_public_repo.py %dst%\premake5.lua
+call "%~dp0..\tools\packman\python" %~dp0edit_public_repo.py %dst%\deps\repo-deps.packman.xml
