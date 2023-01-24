7de505282d8039f1bf724b562b6759263e4ad8b1
Bryan Galdrikian <bgaldrikian@nvidia.com>
2023-01-24 11:32:38 -0800

    Updating copyrights Updating docs list and sdk copy script

--- tools/copy_sdk_to_public_repo.bat
+++ tools/copy_sdk_to_public_repo.bat
@@ -13,13 +13,11 @@ robocopy %~dp0.. %dst% /s /ns /nc /ndl /np ^
 /xd "_*" ".*" "docs" "tools" "exporter" "physx" "RT" "apps" "samples" ^
 /xf "NvBlastExtPx*" "NvBlastExtKJPx*" "ExtPx*" "ext_px*" "ext_physx*" "ext_exporter*" "tinyobjloader-LICENSE.md" "*.rst" "CODEOWNERS"
 robocopy %~dp0..\docs %dst%\docs CHANGELOG.md /ns /nc /ndl /np
-robocopy %~dp0..\include\extensions\physx %dst%\include\extensions\physx NvBlastExtPxTask.h /ns /nc /ndl /np
-robocopy %~dp0..\source\sdk\extensions\physx %dst%\source\sdk\extensions\physx NvBlastExtPxTaskImpl.* /ns /nc /ndl /np
 robocopy %~dp0..\tools\packman %dst%\tools\packman /s /ns /nc /ndl /np
 robocopy %~dp0..\tools\repoman %dst%\tools\repoman /s /ns /nc /ndl /np /xd "_*"
 
-set edit_file_list=(premake5.lua repo.toml deps\repo-deps.packman.xml deps\target-deps.packman.xml)
+set edit_file_list=premake5.lua repo.toml deps\repo-deps.packman.xml deps\target-deps.packman.xml
 
-for %file in (%edit_file_list%) do (
-    call "%~dp0..\tools\packman\python" %~dp0edit_public_repo.py %dst%\%file%
+for %%f in (%edit_file_list%) do (
+    call "%~dp0..\tools\packman\python" %~dp0edit_public_repo.py %dst%\%%f
 )
