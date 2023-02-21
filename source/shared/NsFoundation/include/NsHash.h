87d42227e34bc375d601caa6be00715a89413c77
Bryan Galdrikian <bgaldrikian@nvidia.com>
2023-02-21 10:02:32 -0800

    Operation copyright 2023

--- source/shared/NsFoundation/include/NsHash.h
+++ source/shared/NsFoundation/include/NsHash.h
@@ -23,8 +23,8 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Copyright (c) 2008-2023 NVIDIA Corporation. All rights reserved.
-// Copyright (c) 2004-2008 AGEIA Technologies, Inc. All rights reserved.
-// Copyright (c) 2001-2004 NovodeX AG. All rights reserved.
+// Copyright (c) 2004-2023 AGEIA Technologies, Inc. All rights reserved.
+// Copyright (c) 2001-2023 NovodeX AG. All rights reserved.
 
 #ifndef NV_NSFOUNDATION_NSHASH_H
 #define NV_NSFOUNDATION_NSHASH_H
